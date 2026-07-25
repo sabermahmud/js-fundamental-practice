f6b3d8de6b6b1ce67518358929fdb248d4079f2f
sabermahmud <sabermahmud01@gmail.com>
2026-07-25 04:16:44 +0300

    git update: problem 2 & 3 is solved

--- day-1/scripts/script.js
+++ day-1/scripts/script.js
@@ -1,4 +1,6 @@
-/*নিচের object থেকে name, price, এবং brand console-এ print করো।
+/*
+Problem 1 — Property Access
+নিচের object থেকে name, price, এবং brand console-এ print করো।
 const laptop = {
     name: "Modern 14",
     brand: "MSI",
@@ -10,9 +12,25 @@ const laptop = {
     brand: "MSI",
     price: 3200
 }
-const { name, brand, price } = laptop;
-console.log(`
-    Model: ${name}
-    Brand: ${brand}
-    Price: ${price}
-    `)
+// const { name, brand, price } = laptop;
+// console.log(`
+//     Model: ${name}
+//     Brand: ${brand}
+//     Price: ${price}
+//     `)
+// p2 ************************************
+/*
+Problem 2 — Update Property
+price-কে 3500 করো।
+তারপর নতুন একটি property যোগ করো:
+*/
+// laptop.price = 3500;
+// console.log(laptop)
+
+// p3************************************
+/*
+Problem 3 — Delete Property
+Object থেকে brand property delete করো।
+*/
+// delete laptop.brand;
+// console.log(laptop)
